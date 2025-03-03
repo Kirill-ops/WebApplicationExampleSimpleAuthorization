945e00d2b511165e528015c6440085f01d2d0baa
Kirill <70739729+Kirill-ops@users.noreply.github.com>
2025-03-04 00:44:26 +0900

    --

--- WebApplicationExampleSimpleAuthorization/Controllers/EmployeesController.cs
+++ WebApplicationExampleSimpleAuthorization/Controllers/EmployeesController.cs
@@ -6,7 +6,7 @@ using WebApplicationExampleSimpleAuthorization.Models;
 namespace WebApplicationExampleSimpleAuthorization.Controllers;
 
 [Route("employees")]
-[Authorize (Roles = "EMPLOYEE")] // Если ролей несколько, то они просто через запятую перечисляются
+[Authorize(Roles = "EMPLOYEE")] // Если ролей несколько, то они просто через запятую перечисляются
 public class EmployeesController : Controller
 {
     // Вместо БД
@@ -14,38 +14,20 @@ public class EmployeesController : Controller
         new() { Id = Guid.Empty, Login = "login1", PasswordHash = "pass1" },
         new() { Login = "login2", PasswordHash = "pass2" }];
 
-    // Просто другой класс не хочу создавать, так что будет такой же
-    private readonly List<Employee> _admins = [
-        new() { Id = Guid.Empty, Login = "admin", PasswordHash = "pass1" }];
-
     [HttpGet("")]
     public string GetLogin()
     {
         var claimId = User.FindFirst("USER_ID")
             ?? throw new Exception();
-        if (!Guid.TryParse(claimId.Value, out Guid userId)) 
+        if (!Guid.TryParse(claimId.Value, out Guid userId))
             throw new Exception();
 
         var claimRole = User.FindFirst(ClaimsIdentity.DefaultRoleClaimType)
             ?? throw new Exception();
         var role = claimRole.Value;
+        var employ = _employees.FirstOrDefault(x => x.Id == userId)
+            ?? throw new Exception("Not found");
 
-        if (role == "EMPLOYEE")
-        {
-            var employ = _employees.FirstOrDefault(x => x.Id == userId)
-                ?? throw new Exception("Not found");
-
-            return employ.Login;
-        }
-
-        if (role == "ADMIN")
-        {
-            var admin = _admins.FirstOrDefault(x => x.Id == userId)
-                ?? throw new Exception("Not found");
-
-            return admin.Login;
-        }
-
-        return "Not found!";
+        return employ.Login;
     }
 }
